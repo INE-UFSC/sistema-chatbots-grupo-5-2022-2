896751ca5fe97571988aa8254c7bc2309a826504
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-09-30 10:07:28 -0300

    Criar bot triste

---
+++ Bots/BotTriste.py
@@ -0,0 +1,21 @@
+from Bots.Bot import Bot
+
+COMANDOS = {'Bom dia': 'Bom dia? Amigo, só dia, né, porque de bom não tem nada',
+            'Qual o seu nome?': 'Eu sou o bot triste 😢', 'Quero um conselho': 'Desista', 'Adeus': 'Deus não existe'}
+
+
+class BotTriste(Bot):
+    def __init__(self):
+        super().__init__('Bot Triste', COMANDOS.values())
+
+    def executa_comando(self, cmd):
+        try:
+            return COMANDOS[list(COMANDOS)[int(cmd) - 1]]
+        except (ValueError, KeyError):
+            return 'Eu nem tenho esse comando, tu acha que eu sou o que 😢'
+
+    def boas_vindas(self):
+        return 'Sai daqui mermão não te chamei 😢'
+
+    def despedida(self):
+        return COMANDOS['Adeus']
