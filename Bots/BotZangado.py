97277109caedb14aae153eb792e0ab0ab944ec83
Ricardo H <106935635+RicardoHmedeiros@users.noreply.github.com>
2022-09-30 11:08:18 -0300

    Update BotZangado.py
    Implementado o bot zangado

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -1,28 +1,24 @@
-from Bots.Bot import Bot
+from Bot import Bot
+
+Comandos = {'Bom dia':'Que bom dia oque nem falei com você', 'Qual seu nome': 'Sou o bot zangado e poderia ir embora!', 'Quero um conselho': 
+            'Não incomode as pessoas!', 'Adeus': 'Se nem dei bom dia você acha que vou me despedir?'}
 
 class BotZangado(Bot):
-    def __init__(self,nome):
-        self.__nome = nome
+    def __init__(self):
+        dict_comandos = dict()
+        for i, comando in enumerate(Comandos):
+            dict_comandos.update({str(i + 1): comando})
+        super().__init__('Bot Zangado', dict_comandos)
 
-    #nao esquecer o decorator
-    def nome(self):
-        pass
-
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
-
-    def apresentacao(self):
-        pass
- 
-    def mostra_comandos(self):
-        pass
-    
     def executa_comando(self,cmd):
-        pass
-
-    def boas_vindas(self):
-        pass
+        try:
+            return Comandos[list(Comandos)[int(cmd) - 1]]
+        except(ValueError, KeyError):
+            return 'Oque você tá falando? para de incomodar'
 
     def despedida(self):
-        pass
+        return 'Ainda bem que vai embora não aguentava mais'
+    
+    def boas_vindas(self):
+        return 'Oque você está fazendo aqui vai embora?
+        
