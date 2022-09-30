91f66d9acabc919e7f6d3e15f9c344445aac260d
MarcosLacava <108195797+MarcosLacava@users.noreply.github.com>
2022-09-30 10:13:41 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,6 @@
+from Bots.Bot import Bot
+from SistemaChatBot.SistemaChatBot import SistemaChatBot
+from Bots.BotTriste import BotTriste
+
+sistema = SistemaChatBot("Grupo 5", BotTriste())
+sistema.inicio()
